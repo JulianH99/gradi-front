afd0a561abbfb73ad1c741527370cfdb79d4f51a
JulianH99 <juliancorredor99@gmail.com>
2022-02-26 16:36:40 -0500

    feat(product) added base product info and data fetching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
+import { formatNumber } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'gradi-front';
+export class AppComponent implements OnInit, OnDestroy {
+  productInfo!: any;
+
+  subscription!: Subscription;
+
+  quantity = 1;
+
+  totalPrice = 0;
+
+  selectedColor = '';
+  selectedSize = '';
+
+  constructor(private readonly productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.subscription = this.productService.getProductInfo().subscribe({
+      next: (info) => {
+        this.productInfo = info;
+        this.productInfo.price_format = formatNumber(
+          this.productInfo.price / 100,
+          'en-US',
+          '1.2'
+        );
+        this.productInfo.compare_at_price_format = formatNumber(
+          this.productInfo.compare_at_price / 100,
+          'en-US',
+          '1.2'
+        );
+
+        this.totalPrice = this.productInfo.price * this.quantity;
+      },
+    });
+  }
+
+  get totalPriceFormat() {
+    return formatNumber(this.totalPrice / 100, 'en-US', '1.2');
+  }
+
+  get canAddToCart() {
+    return this.selectedColor && this.selectedSize;
+  }
+
+  increaseQuantity() {
+    this.quantity += 1;
+  }
+
+  decreaseQuantity() {
+    if (this.quantity > 1) {
+      this.quantity -= 1;
+    }
+  }
+
+  addToCart() {
+    const variants = this.productInfo.variants;
+
+    const selectedVariant = variants.find(
+      (variant: any) =>
+        variant.options[0] === this.selectedColor &&
+        variant.options[1] === this.selectedSize
+    );
+
+    console.log(selectedVariant);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MobileSliderComponent } from './components/mobile-slider/mobile-slider.component';
+import { FormsModule } from '@angular/forms';
+import { DesktopSliderComponent } from './desktop-slider/desktop-slider.component';
+import { AddToCardModalComponent } from './add-to-card-modal/add-to-card-modal.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MobileSliderComponent, DesktopSliderComponent, AddToCardModalComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
